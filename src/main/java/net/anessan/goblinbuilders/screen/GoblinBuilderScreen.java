febfa174391bdb236b954dd0d4fa4a8f4d105ffd
Anessan <85826627+OneJoil@users.noreply.github.com>
2024-03-24 23:13:43 +0300

    Revert "to commit 2"
    This reverts commit 3402d3f7b3791b378320e9330bea380a5465bddf.

--- src/main/java/net/anessan/goblinbuilders/screen/GoblinBuilderScreen.java
+++ src/main/java/net/anessan/goblinbuilders/screen/GoblinBuilderScreen.java
@@ -2,38 +2,66 @@ package net.anessan.goblinbuilders.screen;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.anessan.goblinbuilders.GoblinBuilders;
-import net.anessan.goblinbuilders.entity.custom.GoblinBuilderEntity;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
+import java.util.HashMap;
 
 public class GoblinBuilderScreen extends AbstractContainerScreen<GoblinBuilderMenu> {
-    private static final ResourceLocation TEXTURE = new ResourceLocation("goblinbuildersproject:textures/screens/goblin_builder_gui.png");
+    private final static HashMap<String, Object> guistate = GoblinBuilderMenu.guistate;
+    private static final ResourceLocation texture = new ResourceLocation("goblinbuilders:textures/screens/goblin_builder_gui.png");
     private static final Component FOLLOWME_BUTTON = Component.translatable("gui.goblinbuildersproject.goblin_builder_gui.button_follow_me");
-    private final GoblinBuilderEntity goblinBuilder;
-    Button button;
+    private final Level world;
+    private final int x, y, z;
+    private final Player entity;
+    private Button followMeButton;
 
-    public GoblinBuilderScreen(GoblinBuilderMenu pMenu, Inventory pPlayerInventory, GoblinBuilderEntity pGoblinbuilder) {
-        super(pMenu, pPlayerInventory, pGoblinbuilder.getDisplayName());
-        this.goblinBuilder = pGoblinbuilder;
+    public GoblinBuilderScreen(GoblinBuilderMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
+        super(pMenu, pPlayerInventory, pTitle);
+        this.world = pMenu.world;
+        this.x = pMenu.x;
+        this.y = pMenu.y;
+        this.z = pMenu.z;
+        this.entity = pMenu.entity;
+        this.imageWidth = 176;
+        this.imageHeight = 166;
     }
 
-    @Override
-    protected void renderBg(GuiGraphics pGuiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
+    public void init() {
+        super.init();
+        this.followMeButton = addRenderableWidget(
+            Button.builder(
+                FOLLOWME_BUTTON,
+                this::handleFollowMeButton)
+                .bounds(this.leftPos + 8, this.topPos + 11, 65, 20)
+                .build());
+    }
+
+    protected void renderBg(GuiGraphics guiGraphics, float v, int i, int i1) {
         RenderSystem.setShaderColor(1, 1, 1, 1);
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
-        pGuiGraphics.blit(TEXTURE, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight, this.imageWidth, this.imageHeight);
+        guiGraphics.blit(texture, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight, this.imageWidth, this.imageHeight);
         RenderSystem.disableBlend();
     }
 
-    public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
-        this.renderBackground(pGuiGraphics);
-        super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
-        this.renderTooltip(pGuiGraphics, pMouseX, pMouseY);
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+        this.renderBackground(guiGraphics);
+        super.render(guiGraphics, mouseX, mouseY, partialTicks);
+        this.renderTooltip(guiGraphics, mouseX, mouseY);
+    }
+
+    public void containerTick() {
+        super.containerTick();
+    }
+
+    protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
     }
 
     public boolean keyPressed(int key, int b, int c) {
@@ -44,18 +72,12 @@ public class GoblinBuilderScreen extends AbstractContainerScreen<GoblinBuilderMe
         return super.keyPressed(key, b, c);
     }
 
-    public void init() {
-        super.init();
-        this.button = addRenderableWidget(
-            Button.builder(
-                FOLLOWME_BUTTON,
-                this::handleFollowMeButton)
-                .bounds(this.leftPos + 8, this.topPos + 11, 65, 20)
-                .build());
+    public void onClose() {
+        super.onClose();
     }
 
-    private void handleFollowMeButton(Button pButton) {
+    private void handleFollowMeButton(Button button) {
         GoblinBuilders.PACKET_HANDLER.sendToServer(new GoblinBuilderGuiButtonMessage(0, x, y, z));
-        GoblinBuilderGuiButtonMessage.handleFollowMeButton(entity, 0, x, y, z);
+        GoblinBuilderGuiButtonMessage.handleFollowMe(entity, 0, x, y, z);
     }
 }
